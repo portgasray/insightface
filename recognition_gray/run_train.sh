025f9e5bae394555595514507374a66916d9bbbb
portgasray <137861449@qq.com>
2019-05-06 17:03:16 +0800

    add training script and gray scale training

---
+++ recognition_gray/run_train.sh
@@ -0,0 +1,13 @@
+cp sample_config.py config.py 
+mkdir logs && touch logs/test_train_gray.log
+export MXNET_CPU_WORKER_NTHREADS=56
+export MXNET_ENGINE_TYPE=ThreadedEnginePerDevice
+logf="test_train_gray.log"
+CUDA_VISIBLE_DEVICES='4,5' nohup python -u train.py \
+     --network m1 \
+     --loss softmax \
+     --lr-steps="200000, 320000, 440000" \
+     --dataset emore > ./logs/$logf 2>&1 &
+
+tail -f logs/$logf
+
